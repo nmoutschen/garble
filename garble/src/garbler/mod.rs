e407d8c3d59ac242a4e7936647abd1c97b9b876c
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2022-02-22 22:25:40 +0100

    feat: add Garbler::garble

--- garble/src/garbler/mod.rs
+++ garble/src/garbler/mod.rs
@@ -1,3 +1,4 @@
+use crate::Garble;
 use paste::paste;
 
 #[cfg(feature = "simple")]
@@ -9,7 +10,6 @@ macro_rules! garble_func {
     ($($t:ty),*) => {
         $(
             paste! {
-                /// Garble a(n) $t
                 fn [<garble_ $t:lower>](&mut self, value: $t) -> $t;
             }
         )*
@@ -17,7 +17,18 @@ macro_rules! garble_func {
 }
 
 /// Trait for something that can garble data
+#[allow(missing_docs)]
 pub trait Garbler<'g>: Sized {
+    /// Convenience function for garbling a value
+    ///
+    /// This is equivalent to using [`Garble::garble`] and passing this `Garbler`.
+    fn garble<T>(&mut self, value: T) -> T::Output
+    where
+        T: Garble<'g>,
+    {
+        value.garble(self)
+    }
+
     garble_func!(
         bool, u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize, f32, f64, char,
         String
